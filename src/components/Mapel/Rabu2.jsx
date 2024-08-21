e41a177bc2d38e992f1ae15ed64ccc3450a7b160
Mansioqt <153075222+Mansioqt@users.noreply.github.com>
2024-08-21 19:37:56 +0700

    Update Rabu2.jsx

--- src/components/Mapel/Rabu2.jsx
+++ src/components/Mapel/Rabu2.jsx
@@ -13,7 +13,7 @@ const Rabu = () => {
 					<div className="w-[50%] text-base  font-medium">ISTIRAHAT</div>
 					<div className="flex justify-center items-center text-sm">10.00-10.30</div>
 				</div>
-				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
 					<div className="w-[50%] text-base  font-medium">Istirahat</div>
 					<div className="flex justify-center items-center text-sm">11.50-12.30</div>
 				</div>
@@ -23,7 +23,7 @@ const Rabu = () => {
 					<div className="flex justify-center items-center text-sm">12.30-15.00</div>
 				</div>
 		
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
 					<div className="w-[50%] text-base  font-medium">IPAS2</div>
 					<div className="flex justify-center items-center text-sm">12.30-15.00</div>
 				</div>
