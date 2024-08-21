7a96c0395cdd2b5b97990e37056b7542fb4787c4
Mansioqt <153075222+Mansioqt@users.noreply.github.com>
2024-08-21 19:14:38 +0700

    Update Rabu2.jsx

--- src/components/Mapel/Rabu2.jsx
+++ src/components/Mapel/Rabu2.jsx
@@ -26,6 +26,10 @@ const Rabu = () => {
 					<div className="w-[50%] text-base font-medium">Sholat</div>
 					<div className="flex justify-center items-center text-sm">15.00-Selesai</div>
 				</div>	
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Sholat1</div>
+					<div className="flex justify-center items-center text-sm">15.00-Selesai</div>
+				</div>	
         </>
     )
 } 
