764a99fe20f955fd46173baef2c16b594e9ce669
Mansioqt <153075222+Mansioqt@users.noreply.github.com>
2024-08-21 19:12:27 +0700

    Update Rabu2.jsx

--- src/components/Mapel/Rabu2.jsx
+++ src/components/Mapel/Rabu2.jsx
@@ -2,16 +2,16 @@ const Rabu = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">TKJ LX</div>
-					<div className="flex justify-center items-center text-sm">07.30-10.00</div>
+					<div className="w-[50%] text-base  font-medium">B.INGGRIS</div>
+					<div className="flex justify-center items-center text-sm">07.20-09.20</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">10.00-10.30</div>
+					<div className="w-[50%] text-base  font-medium">MATEMATIKA</div>
+					<div className="flex justify-center items-center text-sm">09-11.50</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">TKJ LX</div>
-					<div className="flex justify-center items-center text-sm">10.30-11.50</div>
+					<div className="w-[50%] text-base  font-medium">ISTIRAHAT</div>
+					<div className="flex justify-center items-center text-sm">10.00-10.30</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
 					<div className="w-[50%] text-base  font-medium">Istirahat</div>
